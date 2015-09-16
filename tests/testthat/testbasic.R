b2bf7e0b0f7e4af88d6c7ad6f48127a6b74b799e
Michael Lauer <mrlauer@gmail.com>
2015-09-16 19:12:08 -0400

    more test

--- tests/testthat/testbasic.R
+++ tests/testthat/testbasic.R
@@ -13,10 +13,28 @@ test_that("Basic methods appear to work", {
     N <- 5
     m <- matrix(rnorm(N*N), nrow = N)
     cm <- makeCacheMatrix(m)
-    inv <- cacheSolve(cm)
-    expect_is(inv, "matrix")
-    expect_equal(nrow(inv), 5)
 
-    product <- m %*% inv
-    expect_equal(product, diag(N))
+    for (i in 1:2) {
+        inv <- cacheSolve(cm)
+        expect_is(inv, "matrix")
+        expect_equal(nrow(inv), 5)
+
+        product <- m %*% inv
+        expect_equal(product, diag(N))
+    }
+})
+
+test_that("Caching saves some time", {
+    N <- 1000
+    m <- matrix(rnorm(N*N), nrow = N)
+    cm <- makeCacheMatrix(m)
+
+    times <- lapply(as.list(1:2), function(i) {
+        inv <- NULL
+        t <- system.time(inv <- cacheSolve(cm))
+        expect_is(inv, "matrix")
+        expect_equal(nrow(inv), N)
+        t
+    })
+    expect_true(times[[1]]["user.self"] > times[[2]]["user.self"]*100)
 })
